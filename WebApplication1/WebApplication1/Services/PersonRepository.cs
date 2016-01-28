ba946e488be03e635961dc389d17bbed4327e657
bartek.sikorski@gmail.com <bartek.sikorski@gmail.com>
2016-01-28 21:57:06 +0100

    aaa

---
+++ WebApplication1/WebApplication1/Services/PersonRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace WebApplication1.Services
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private WebAppContext db;
+
+        public PersonRepository()
+        {
+            db = new WebAppContext();
+
+    }
+    public IEnumerable<PersonVM> getAllPeople()
+        {
+          
+
+            return db.People.Select(p => new PersonVM
+            {
+                Firstname = p.FirsName,
+                Surname = p.Surname,
+                Birthdate = p.Birthdate,
+                Addresses = p.Addresses.Select(a => new AddressVM
+                {
+                    City = a.City,
+                    HouseNo = a.HouseNo,
+                    FlatNo = a.FlatNo,
+                    Street = a.Street,
+                    ZipCode = a.ZipCode
+
+                }).ToList()
+            });
+  
+        }
+
+        public PersonVM getPersonById(int PersonID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
