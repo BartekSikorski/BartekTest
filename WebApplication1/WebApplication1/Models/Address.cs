afd953d32e4bae9ef376b7f02224001cbd255225
bartek.sikorski@gmail.com <bartek.sikorski@gmail.com>
2016-01-28 18:12:36 +0100

    Nowe klasy w modelu

---
+++ WebApplication1/WebApplication1/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public int HouseNo { get; set; }
+        public int FlatNo { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public AddressType Type { get; set; }
+
+        public Person Person { get; set; }
+    }
+
+    public enum AddressType
+    {
+        zameldowania,
+        korespondencyjny
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            Addresses = new HashSet<Address>();
+        }
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public ICollection<Address> Addresses { get; set; }
+
+
+    }
+}
